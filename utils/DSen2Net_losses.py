17c6d593e36c8cffe63ff1950fec2ce0c1dc5be7
David Ryglicki <david@acmeaom.com>
2022-11-30 19:22:17 -0500

    Adding MAE+DSSIM loss, plus typo fixing.

--- utils/DSen2Net_losses.py
+++ utils/DSen2Net_losses.py
@@ -21,3 +21,24 @@ class MSE_and_DSSIM(K.losses.Loss):
             'weight_dssim' : self.weight_dssim
             })
         return config
+
+class MAE_and_DSSIM(K.losses.Loss):
+    def __init__(self, weight_mae = 1.0, weight_dssim = 0.5, max_val=1.0, **kwargs):
+        super(MAE_and_DSSIM, self).__init__(**kwargs)
+        self.weight_mae = weight_mae
+        self.weight_dssim = weight_dssim
+        self.max_val = max_val
+
+    def call(self, y_true, y_pred):
+        mae = self.weight_mae * tf.reduce_mean(tf.math.abs(y_pred - y_true))
+        dssim = self.weight_dssim * (1. - tf.image.ssim(y_true, y_pred, max_val=self.max_val))
+        return mae + dssim
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'weight_mae' : self.weight_mae,
+            'weight_dssim' : self.weight_dssim,
+            'max_val' : self.max_val
+            })
+        return config
